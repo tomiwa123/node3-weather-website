988f42eb11cd0a843eeecd761a7a8764fe58ed10
Ayotomiwa Akinyele <ayotomiwa.akinyele@gmail.com>
2019-07-20 22:34:11 +0100

    Remove unnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log("Client Javascript loaded");
-
-
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector('input');
 const messageOne = document.querySelector("#message-1");
