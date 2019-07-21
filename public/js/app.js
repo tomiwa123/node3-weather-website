5a2251a769e7bfc81b38ea5e862abdd1eab727fc
Ayotomiwa Akinyele <ayotomiwa.akinyele@gmail.com>
2019-07-22 00:46:38 +0100

    Setup App for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-const weatherForm = document.querySelector("form");
+ const weatherForm = document.querySelector("form");
 const search = document.querySelector('input');
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...';
     messageTwo.textContent = '';
     
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error;
